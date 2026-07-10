407dce10e3352841c16ab0abe7cddefe6edae744
Fabio Gaming <fabio.furch@gmail.com>
2026-07-10 10:09:03 +0200

    fix(database): rename createdAt field in guilds schema
    - Changed field name from 'added_at' to 'created_at' for consistency.

--- src/database/guilds.schema.ts
+++ src/database/guilds.schema.ts
@@ -3,7 +3,7 @@ import { bigint, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 export const guilds = pgTable('guilds', {
     id: uuid('id').primaryKey().defaultRandom(),
     guildId: text('guild_id').notNull(),
-    createdAt: timestamp('added_at', {
+    createdAt: timestamp('created_at', {
         mode: 'date',
         withTimezone: false,
     }).defaultNow(),
