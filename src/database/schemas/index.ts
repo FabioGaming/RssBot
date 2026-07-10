da2bb6f1ee4041cad8d94b89314a59bca3ec7706
Fabio Gaming <fabio.furch@gmail.com>
2026-07-10 10:46:54 +0200

    feat(database): export delivery_logs schema in index file
    - Added export for delivery_logs schema to the index file.

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -2,3 +2,4 @@ export * from './guilds.schema';
 export * from './feeds.schema';
 export * from './feed-items.schema';
 export * from './subscriptions.schema';
+export * from './delivery-logs.schema';
